237afe6d05643b478251bb5b6a4a3ad09d7c8b38
jaychung <jay@max2.com>
2017-01-10 21:25:50 -0500

    make pretty button and link to amazon

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,13 @@
 import	React from 'react';
 import	{Text, TouchableOpacity} from 'react-native';
 
-const Button = ({onPress}) => {
+const Button = ({ onPress, children }) => {
 	const {buttonStyle, textStyle} = styles;
 
 	return(
 		<TouchableOpacity style={buttonStyle}
 			onPress = {onPress} >
-			<Text style={textStyle}> Click me!!</Text>
+			<Text style={textStyle}> {children}</Text>
 		</TouchableOpacity>
 	);
 };
